f9d249280ddca73081f6b0e55b04887fc7ceab2e
Keeperjoey <Keeperjoey@hotmail.com>
2016-04-25 10:58:32 +0200

    download

--- DataVisualisatieDev8/src/main/java/jsonDownloader.java
+++ DataVisualisatieDev8/src/main/java/jsonDownloader.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -14,28 +13,23 @@ import java.net.URL;
  */
 public class jsonDownloader {
 
-    String jsonDownloader(String urlString) throws Exception {
-       String jsonString = jsonDownload(urlString);
-       return jsonString;
-    }
-
-
-    
     public String jsonDownload(String urlString) throws Exception {
-    BufferedReader reader = null;
-    try {
-        URL url = new URL(urlString);
-        reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        StringBuffer buffer = new StringBuffer();
-        int read;
-        char[] chars = new char[1024];
-        while ((read = reader.read(chars)) != -1)
-            buffer.append(chars, 0, read); 
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1) {
+                buffer.append(chars, 0, read);
+            }
 
-        return buffer.toString();
-    } finally {
-        if (reader != null)
-            reader.close();
+            return buffer.toString();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
     }
-}
 }
