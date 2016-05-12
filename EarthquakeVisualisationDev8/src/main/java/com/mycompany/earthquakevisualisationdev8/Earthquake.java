8dd787ef2b8c2bb47bcb033fa7957912e3e2ddc8
RoyvandenHeuvel <0878963@hr.nl>
2016-05-12 14:48:41 +0200

    update applicationn

---
+++ EarthquakeVisualisationDev8/src/main/java/com/mycompany/earthquakevisualisationdev8/Earthquake.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.earthquakevisualisationdev8;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Roy van den Heuvel
+ */
+public class Earthquake {
+    private Timestamp timestamp;
+    private Double latitude;
+    private Double longitude;
+    private Double depth;
+    private Double size;
+    private Double quality;
+    private String humanReadableLocation;
+    
+    public Earthquake(){
+        
+    }
+
+    public Earthquake(Timestamp timestamp, Double latitude, Double longitude, Double depth, Double size, Double quality, String humanReadableLocation) {
+        this.timestamp = timestamp;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.depth = depth;
+        this.size = size;
+        this.quality = quality;
+        this.humanReadableLocation = humanReadableLocation;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(Double depth) {
+        this.depth = depth;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public Double getQuality() {
+        return quality;
+    }
+
+    public void setQuality(Double quality) {
+        this.quality = quality;
+    }
+
+    public String getHumanReadableLocation() {
+        return humanReadableLocation;
+    }
+
+    public void setHumanReadableLocation(String humanReadableLocation) {
+        this.humanReadableLocation = humanReadableLocation;
+    }
+}
